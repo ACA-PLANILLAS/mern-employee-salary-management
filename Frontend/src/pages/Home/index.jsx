8223a81761489348516947ab07c6ef2ecef5a1e3
MelvinAguilar <00067621@uca.edu.sv>
2025-05-18 17:18:28 -0600

    :bug: Fixing issue where login errors were appearing when entering

--- Frontend/src/pages/Home/index.jsx
+++ Frontend/src/pages/Home/index.jsx
@@ -5,32 +5,41 @@ import Contact from "../Contact";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { getMe } from "../../config/redux/action";
+import { API_URL } from "@/config/env";
+import axios from "axios";
 
 const Home = () => {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const { user, isError } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { user, isError } = useSelector((state) => state.auth);
 
-    useEffect(() => {
-        dispatch(getMe());
-    }, [dispatch]);
+  useEffect(() => {
+    axios
+      .get(`${API_URL}/me`)
+      .then((res) => {
+        navigate("/dashboard");
+      })
+      .catch((err) => {
+        console.log("no hay sesión activa");
+      });
+  }, []);
 
-    useEffect(() => {
-        if (user) {
-            navigate('/dashboard');
-        }
-    }, [isError, user, navigate]);
+  useEffect(() => {
+    if (user) {
+      navigate("/dashboard");
+    }
+  }, [isError, user, navigate]);
 
-    return (
-        <>
-            <div className="dark:bg-boxdark">
-                <Navbar />
-                <Banner />
-                <About />
-                <Contact />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="dark:bg-boxdark">
+        <Navbar />
+        <Banner />
+        <About />
+        <Contact />
+      </div>
+    </>
+  );
 };
 
 export default Home;
