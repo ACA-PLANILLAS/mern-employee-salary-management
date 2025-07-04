2d3ee68f2885abac927571bba73a066825397f1e
HenryLima07 <00033721@uca.edu.sv>
2025-07-04 14:11:02 -0600

    update swagger config

--- Backend/config/swagger.js
+++ Backend/config/swagger.js
@@ -1,39 +1,39 @@
-   // Backend/config/swagger.js
-   import swaggerJSDoc from 'swagger-jsdoc';
-   import { fileURLToPath } from 'url';
-   import { dirname, join } from 'path';
+// Backend/config/swagger.js
+import swaggerJSDoc from 'swagger-jsdoc';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
-   const __filename = fileURLToPath(import.meta.url);
-   const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-   const options = {
-     definition: {
-       openapi: '3.0.0',
-       info: {
-         title: 'API de Gestión de Empleados',
-         version: '1.0.0',
-         description: 'Documentación de la API con Swagger',
-       },
-       servers: [
-         {
-           url: 'http://localhost:5000',
-           description: 'Servidor de desarrollo',
-         },
-       ],
-       components: {
-         securitySchemes: {
-           sessionAuth: {
-             type: 'apiKey',
-             in: 'cookie',
-             name: 'connect.sid',
-             description: 'Autenticación por sesión'
-           }
-         }
-       }
-     },
-     apis: [join(__dirname, '../routes/*.js'), join(__dirname, '../models/*.js')],
-   };
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API de Gestión de Empleados',
+      version: '1.0.0',
+      description: 'Documentación de la API con Swagger',
+    },
+    servers: [
+      {
+        url: 'https://mern-backend-prod-283773467884.us-central1.run.app',
+        description: 'Servidor de desarrollo',
+      },
+    ],
+    components: {
+      securitySchemes: {
+        sessionAuth: {
+          type: 'apiKey',
+          in: 'cookie',
+          name: 'connect.sid',
+          description: 'Autenticación por sesión'
+        }
+      }
+    }
+  },
+  apis: [join(__dirname, '../routes/*.js'), join(__dirname, '../models/*.js')],
+};
 
-   const swaggerSpec = swaggerJSDoc(options);
+const swaggerSpec = swaggerJSDoc(options);
 
-   export default swaggerSpec;
+export default swaggerSpec;
